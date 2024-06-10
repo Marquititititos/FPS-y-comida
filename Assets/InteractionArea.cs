517e60db83be06ca76fdc8467a726c6095f623f3
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-06-10 15:57:47 -0300

    Initial commit

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text texto;
+    int puntos = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        texto.text = puntos.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("COLISIONASTE");
+
+        PickupScript pickupScript = other.gameObject.GetComponent<PickupScript>();
+        puntos += pickupScript.puntos;
+
+        if (pickupScript.esComida == true)
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
